97ccf88babc58414d193ae1cfeded91e5186c197
王睿昊 <u46025624@163.com>
2023-09-12 20:41:57 +0800

    Update OpenCV

--- HomomorphicFiltering/opencv_1.cpp
+++ HomomorphicFiltering/opencv_1.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 using namespace cv;
-
+// 应用同态滤波器的函数
 Mat HomoFilter(cv::Mat src) {
+    // 将原始图像转换为双精度
     src.convertTo(src, CV_64FC1);
+
+    // 检查并调整图像大小为偶数
     int rows = src.rows;
     int cols = src.cols;
     int m = rows % 2 == 1 ? rows + 1 : rows;
@@ -12,8 +15,11 @@ Mat HomoFilter(cv::Mat src) {
     copyMakeBorder(src, src, 0, m - rows, 0, n - cols, BORDER_CONSTANT, Scalar::all(0));
     rows = src.rows;
     cols = src.cols;
+
+    // 创建用于存储滤波后图像的目标矩阵
     Mat dst(rows, cols, CV_64FC1);
-    // 1. ln
+
+    // 1.对数变换
     for (int i = 0; i < rows; i++) {
         double* srcdata = src.ptr<double>(i);
         double* logdata = src.ptr<double>(i);
@@ -21,9 +27,10 @@ Mat HomoFilter(cv::Mat src) {
             logdata[j] = log(srcdata[j] + 0.0001);
         }
     }
-    // 2. dct
+    // 2.进行离散余弦变换（DCT)
     Mat mat_dct = Mat::zeros(rows, cols, CV_64FC1);
     dct(src, mat_dct);
+
     // 3. 高斯同态滤波器
     Mat H_u_v;
     double gammaH = 2.0;
@@ -39,11 +46,13 @@ Mat HomoFilter(cv::Mat src) {
             dataH_u_v[j] = (gammaH - gammaL) * (1 - exp(-C * d2 / d0)) + gammaL;
         }
     }
-    H_u_v.ptr<double>(0)[0] = 1.1;
+    H_u_v.ptr<double>(0)[0] = 1.0;
     mat_dct = mat_dct.mul(H_u_v);
-    // 4. idct
+
+    // 4.进行逆离散余弦变换（IDCT）
     idct(mat_dct, dst);
-    // exp
+
+    // 指数变换
     for (int i = 0; i < rows; i++) {
         double* srcdata = dst.ptr<double>(i);
         double* dstdata = dst.ptr<double>(i);
@@ -51,13 +60,14 @@ Mat HomoFilter(cv::Mat src) {
             dstdata[j] = exp(srcdata[j]);
         }
     }
+     // 转换为8位无符号整型图像
     dst.convertTo(dst, CV_8UC1);
     return dst;
 }
 
 int main() {
     // 读取原始图像
-    Mat src = cv::imread("input_image.ppm");
+    Mat src = cv::imread("349293-20181010164333331-2001442514.jpg");
     imshow("origin", src);
 
     int originrows = src.rows;
